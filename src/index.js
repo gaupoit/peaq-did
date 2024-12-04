a3fb4d60323afe81f1c295d9692b9f1eb28f05c8
0xGP <gaupoit@vktechventures.com>
2024-12-04 04:37:11 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -6,15 +6,13 @@ dotenv.config();
 
 // Configuration
 const config = {
-  nodeUrl: process.env.PEAQ_NODE_URL || "wss://wss.agung.peaq.network",
+  nodeUrl: process.env.PEAQ_NODE_URL || "wss://wsspc1-qa.agung.peaq.network",
   seed: process.env.MNEMONIC_SEED,
   did: process.env.IGAM3_M1_SERIES_NUMBER,
   connectionTimeout: 15000, // Reduced timeout to 15 seconds
   maxRetries: 3,
   accountInitTimeout: 10000, // 10 seconds timeout for account initialization
 };
-
-console.log("Config:", config);
 
 const createSdkInstance = async ({ nodeUrl, seed }) => {
   try {
@@ -36,7 +34,7 @@ const createDID = async (sdk, nameDID) => {
 
 // Example usage
 const main = async () => {
-  const nameDID = "igam3_m1";
+  const nameDID = config.did;
   // see SDK Quickstart for createSdkInstance() function
   const sdk = await createSdkInstance(config);
   await sdk.connect();
